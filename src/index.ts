a837f8707c74bc91d32fb912a9a3f6f56bf25e9f
dev-nikunj21 <devnikunj21@gmail.com>
2024-12-23 02:02:13 +0530

    test passed for empty string, failed for string

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export default function sum(a: string): number | undefined {
+  if (a.length === 0) return 0;
+}
--- src/tdd.test.ts
+++ src/tdd.test.ts
@@ -1,5 +1,8 @@
+import add from "./index";
+test("should return total of given string", () => {
+  expect(add("")).toBe(0);
+});
 
-
-test("should return total of given string",()=>{
-    expect(add("1")).toBe(1)
-})
+test("should return 0 for empty string", () => {
+    expect(add("")).toBe(0);
+});
